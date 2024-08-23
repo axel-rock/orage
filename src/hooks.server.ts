68a08d07059f71b872a65a50b4d3a49815ec9528
axel-rock <rockaxel@gmail.com>
2024-08-23 08:40:07 +0200

    fix: Set node version

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,9 @@
-import type { Handle } from "@sveltejs/kit";
-import { drizzle } from "drizzle-orm/d1";
+import type { Handle } from '@sveltejs/kit'
+import { drizzle } from 'drizzle-orm/d1'
 
 export const handle: Handle = async ({ event, resolve }) => {
+	if (event.platform) event.locals.db = drizzle(event.platform.env.DB)
 
-  const db = drizzle(event.platform.env.DB);
-
-  event.locals.db = db;
-
-	const response = await resolve(event);
-	return response;
-}; 
+	const response = await resolve(event)
+	return response
+}

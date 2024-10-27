15a5c564e595c3bb607b6d3ef22c636e12573f82
axel-rock <rockaxel@gmail.com>
2024-10-27 14:31:20 +0100

    feat: Update to Svelte 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,21 +3,23 @@
 	import { setContext } from 'svelte'
 	import type { LayoutData } from './$types'
 
-	export let data: LayoutData
-
-	const { user } = data
-
-	setContext('user', user)
-
 	import '/src/css/style.css'
+	interface Props {
+		data: LayoutData
+		children?: import('svelte').Snippet
+	}
+
+	let { data, children }: Props = $props()
+	const { user } = data
+	setContext('user', user)
 </script>
 
 <header>
 	<h1><a href="/">Orage</a></h1>
 
 	<nav>
-		<a href="/">Home</a>
-		<a href="/users">Users</a>
+		<a href="/" role="button">Home</a>
+		<a href="/users" role="button">Users</a>
 		{#if user}
 			<form action="/?/logout" method="POST">
 				<button type="submit">Logout ({user.email})</button>
@@ -30,12 +32,23 @@
 	</nav>
 </header>
 
-<slot />
+<main>
+	{@render children?.()}
+</main>
+
+<footer>
+	<!--  -->
+</footer>
 
 <style>
 	header {
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
+	}
+
+	nav {
+		display: flex;
+		align-items: baseline;
 	}
 </style>
